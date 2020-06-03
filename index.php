bf4dfd77781af79d25b046ec5235d808a693ea9c
IronMan1030 <minidolls1030@gmail.com>
2020-06-03 14:19:23 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+echo "hello world";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <div>
+            <h1>hello world</h1>
+        </div>
+    </div>
+</body>
+</html>
+
+
+
+
